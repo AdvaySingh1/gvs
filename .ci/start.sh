e1f6b27cf46415663464044fad0f79ae931b54cc
advay <advay@umich.edu>
2025-08-04 05:08:31 +0000

    current

--- .ci/start.sh
+++ .ci/start.sh
@@ -4,19 +4,6 @@ export PATH="/usr/local/share/openvswitch/scripts:$PATH"
 
 # Start OVS with userspace datapath
 ovs-ctl --no-ovs-vswitchd --system-id=random --delete-bridges start
-# ovs-vsctl set Open_vSwitch . other_config:max-idle=600000 -- \
-#   set Open_vSwitch . other_config:gigaflow-enable=true -- \
-#   set Open_vSwitch . other_config:gigaflow-lookup-enable=true -- \
-#   set Open_vSwitch . other_config:gigaflow-tables-limit=4 -- \
-#   set Open_vSwitch . other_config:gigaflow-max-entries=8000 -- \
-#   set Open_vSwitch . other_config:gigaflow-max-masks=64 -- \
-#   set Open_vSwitch . other_config:gigaflow-estimate-flow-space=true -- \
-#   set Open_vSwitch . other_config:hw-offload=true -- \
-#   set Open_vSwitch . other_config:hw-offload-p4sdnet=true -- \
-#   set Open_vSwitch . other_config:gigaflow-offload=true -- \
-#   set Open_vSwitch . other_config:n-handler-threads=1 -- \
-#   set Open_vSwitch . other_config:n-revalidator-threads=1 -- \
-#   set Open_vSwitch . other_config:pmd-perf-metrics=true
 
 ovs-vsctl --no-wait \
   set Open_vSwitch . other_config:max-idle=600000 -- \
@@ -45,5 +32,15 @@ ovs-vsctl --if-exists del-br br0
 ovs-vsctl add-br br0
 ovs-vsctl set bridge br0 datapath_type=netdev
 ovs-vsctl set bridge br0 fail-mode=secure
-ovs-vsctl add-port br0 ens16f0
-ovs-vsctl add-port br0 ens16f1
+ovs-vsctl add-port br0 p1
+ovs-vsctl add-port br0 p2
+
+
+# Variables (edit as needed)
+COLLECTOR_OVS_PATH="/tmp/gigaflow/logs/ee/high-locality/4-8000/cord-ofdpa"
+RULES_PATH="/home/advay/gigaflow/ovs-pipelines"
+RULESET="high-locality/cord/ofdpa/of2/10k/ruleset.ovs"
+
+sudo rm -rf "$COLLECTOR_OVS_PATH"
+sudo mkdir -p "$COLLECTOR_OVS_PATH"
+# sudo ovs-ofctl add-flows br0 "$RULES_PATH/$RULESET"
